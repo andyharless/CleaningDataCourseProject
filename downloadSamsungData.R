6414f75fa27fef9283dbf22c30c8174a142f4da4
Andy Harless <a23@harless.us>
2017-03-10 14:11:52 -0500

    Script to download data

---
+++ downloadSamsungData.R
@@ -0,0 +1,5 @@
+# Download and unzip data
+
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
+              "Dataset.zip")
+unzip("Dataset.zip")
